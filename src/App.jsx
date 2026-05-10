c28b1945cdf4bfe0ef32ed78c8579e9237884252
sosMoH <mohislamedu@gmail.com>
2026-05-10 14:08:30 +0300

    Navbar + Assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-sky-500 text-3xl">Hello, GSAP SOS!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,53 @@
+import { useGSAP } from "@gsap/react";
+import { navLinks } from "../../constants";
+import gsap from "gsap";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        // Start Controls start & end: bottom of navbar (REACHES) top of viewport
+        start: "bottom top",
+      },
+    });
+
+    // Params:
+    // 1- Target to animate
+    // 2- Duration
+    // 3- from
+    // 4- to
+    // 5- Position
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+  });
+
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img src="/images/logo.png" alt="" />
+          <p>SOS Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
